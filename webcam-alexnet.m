e2f16c6b67816d141230547efc04270572c93c1b
tklim99 <110584634+tklim99@users.noreply.github.com>
2022-10-29 13:28:02 +0900

    Add files via upload

---
+++ webcam-alexnet.m
@@ -0,0 +1,13 @@
+clear
+
+camera=webcam;
+net = alexnet;
+
+while true
+    im = snapshot(camera);
+    image(im);
+    im=imresize(im,[227 227]);
+    label=classify(net, im);
+    title(char(label));
+    drawnow
+end
